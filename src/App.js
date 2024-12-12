c456ce94541e4cb98b5e27019235a13a5db8ca85
Andrew Cate <andrewjcate@gmail.com>
2024-12-12 14:27:39 -0800

    Progress on Header and Footer components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import { Header, Footer } from './components';
 import { AllRoutes } from './routes/AllRoutes';
 
 function App() {
   return (
-    <div>
-      <AllRoutes />
+    <div className=''>
+      <Header />
+      <div>
+        <AllRoutes />
+      </div>
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,23 @@
+import { Link } from 'react-router-dom';
+
 export const Footer = () => {
   return (
-    <footer>Footer</footer>
+    <footer className="bg-white shadow m-4 dark:bg-gray-800">
+        <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400"><Link to="/" className="hover:underline">Cinemate</Link> 2024
+        </span>
+        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+            <li>
+                <a href="https://github.com/AndrewJCate" target='_blank' className="hover:underline me-4 md:me-6">GitHub</a>
+            </li>
+            <li>
+                <a href="https://www.linkedin.com/in/andrewjcate/" target='_blank' className="hover:underline me-4 md:me-6">LinkedIn</a>
+            </li>
+            <li>
+                <a href="https://andrewjcate.com/" target='_blank' className="hover:underline">Portfolio</a>
+            </li>
+        </ul>
+        </div>
+    </footer>
   )
 }

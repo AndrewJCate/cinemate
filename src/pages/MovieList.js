65ee2998cf23d5877633a136664abf881e59adf8
Andrew Cate <andrewjcate@gmail.com>
2025-01-07 17:11:55 -0800

    Adds MovieList page

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,31 @@
+import { useEffect, useState } from "react"
+import { MovieCard } from "../components/MovieCard"
+
 export const MovieList = () => {
+
+  const BASE_URL = "https://api.themoviedb.org/3/movie/";
+  const KEY = "api_key=789013c2e0390274172c1942a24ec572";
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect( () => {
+    async function fetchMovies() {
+      const response = await fetch(BASE_URL + "now_playing?" + KEY);
+      const data = await response.json();
+      setMovies(data.results);
+    }
+    fetchMovies();
+  }, []);
+
   return (
-    <main>MovieList</main>
+    <main>
+      <section className="py-3">
+        <div className="flex justify-start flex-wrap gap-6 pl-5">
+          { movies.map( (movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          )) }
+        </div>
+      </section>
+    </main>
   )
 }
